849eaf20aa83738c7caaf9ee9c292318f0667f9f
Bacha Wondimu <bwondim3@montgomerycollege.edu>
2024-02-09 21:35:02 -0600

    project has been created

---
+++ RentalCarManagementApp/src/main/java/rentacar/org/rentalcarmgntapp/RentalCarManagementAppApplication.java
@@ -0,0 +1,13 @@
+package rentacar.org.rentalcarmgntapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarManagementAppApplication.class, args);
+    }
+
+}
---
+++ RentalCarManagementApp/src/test/java/rentacar/org/rentalcarmgntapp/RentalCarManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rentacar.org.rentalcarmgntapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
